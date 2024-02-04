7a6401005b6db5b4ee23021bbcbc20cd40459323
Abhinav <abhinavgtechnical@gmail.com>
2024-02-04 18:17:00 +0530

    Fixed the bug in About

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,14 +7,15 @@ const About = () => {
   return (
     <motion.div
       id="about"
-      className="my-4 h-full grid place-items-center relative rounded"
-      initial={{ x: 500, opacity: 0 }}
-      whileInView={{
+      className="my-4 h-screen grid place-items-center relative rounded-lg overflow-hidden"
+      initial={{ x: 300, opacity: 0 }}
+      animate={{
         x: 0,
         opacity: 1,
       }}
       transition={{
         duration: 0.5,
+        delay: 5,
         ease: "easeInOut",
       }}
     >
@@ -25,23 +26,23 @@ const About = () => {
         loop
         className="w-full h-full object-cover absolute aspect-video inset-0 -z-0"
       ></video>
-      <div className="z-10 p-16 px-7 bg-black/30">
+      <div className="z-10 h-full w-full md:p-16 md:px-7 px-0 py-4 bg-black/30 grid place-items-center">
         <h1 className="text-3xl font-black tracking-widest underline-offset-8 text-d_textColor  underline text-center uppercase">
           About Me
         </h1>
-        <div className="flex mt-8 gap-8 p-4 flex-col md:flex-row place-items-center justify-around">
+        <div className="flex w-full relative mt-8 gap-8 md:p-4 p-2 flex-col md:flex-row place-items-center justify-around">
           <img
             className="w-56 h-56 rounded-full shadow-lg shadow-slate-900 transition-all duration-300 hover:-translate-y-8"
             src="/image/avatar.png"
             alt="Profile"
           />
-          <div className="lg:w-2/4 flex flex-col gap-4 text-d_textColor ">
-            <h1 className="text-4xl font-extrabold tracking-tighter">
+          <div className="lg:w-2/4 flex w-full flex-col gap-6 text-d_textColor ">
+            <h1 className="md:text-4xl text-3xl font-extrabold tracking-tighter ">
               Hey, I'M Abhinav Maurya <br />A Full Stack Web Developer <br />
-              <span className="text-4xl font-extrabold tracking-normal text-yellow-400">
-                Frontend | Backend
-              </span>
             </h1>
+            <span className="text-3xl font-extrabold tracking-normal text-yellow-400">
+              Frontend | Backend
+            </span>
             <a
               href="mailto:abhinavmaurya476@gmail.com"
               className="transform transition-transform py-2 px-4 w-40 rounded gap-2 place-items-center font-bold duration-300 hover:-translate-y-1 flex bg-d_primary dark:bg-l_primary text-d_textColor dark:text-l_textColor hover:bg-d_secondary hover:dark:bg-l_secondary"
@@ -50,7 +51,7 @@ const About = () => {
             </a>
           </div>
         </div>
-        <div className="text-center text-wrap">
+        <div className="text-center text-wrap hidden md:block absolute bottom-8">
           <h1 className="text-2xl font-bold tracking-tighter text-yellow-400 uppercase">
             The next big project deserves the right developer. Click{" "}
             <span className="text-black bg-white px-2 rounded text-xl">
